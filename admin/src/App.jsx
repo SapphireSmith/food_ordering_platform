fb48523d6c03a0f6939f4ebdfe59d76acdcd3cb5
Sapphire Smith <ssapphire527@gmail.com>
2024-05-14 13:28:07 +0530

    Added server side url on admin side

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  const url = "http://localhost:5000"
+  const url = "https://food-ordering-platform-5206.onrender.com"
 
   return (
     <div>
